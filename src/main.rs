10cb8c84291a1d1afd3117a23fdb6e63eeaaa27a
Javier Solis <javsolis@mit.edu>
2024-09-24 12:22:53 -0500

    enhance parser with additional, more specific regex matching

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,32 @@ fn main() {
     // Regex to match any line that contains `// todo ... :`
     let general_todo_re = Regex::new(r"//\s*todo.*:").unwrap();
 
+    // Regex to match correctly formatted 'todo' comments
+    let keyword_pattern = r"[a-zA-Z0-9_-]+";
+    let parens_pattern = format!(r"\((?<parens>{})\)", keyword_pattern);
+    let braces_pattern = format!(r"\{{(?<braces>{})\}}", keyword_pattern);
+    let brackets_pattern = format!(r"\[(?<brackets>{})\]", keyword_pattern);
+    let angles_pattern = format!(r"<(?<angles>{})>", keyword_pattern);
+    let delimiter_pattern = format!(
+        r"(?:{}|{}|{}|{})",
+        parens_pattern, braces_pattern, brackets_pattern, angles_pattern
+    );
+    let specific_todo_re = Regex::new(&format!(r"//\s*todo{}{{0,3}}:", delimiter_pattern)).unwrap();
+
     // Parse through file to match against the general regex
     for (line_number, line) in contents.lines().enumerate() {
         if let Some(general_cap) = general_todo_re.captures(line) {
             let todo_comment = &general_cap[0];
-            println!("'todo' on line {}:", line_number + 1);
-            println!("\tFull text: {}", todo_comment);
-            println!("\n");
+            // Match against the specific regex
+            if specific_todo_re.is_match(todo_comment) {
+                println!("'todo' with valid format on line {}:", line_number + 1);
+                println!("\tFull text: {}", todo_comment);
+                println!("\n");
+            } else {
+                println!("'todo' with invalid format on line {}:", line_number + 1);
+                println!("\tFull text: {}", todo_comment);
+                println!("\n");
+            }
         }
     }
 }
