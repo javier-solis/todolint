e0e090bebdd132f150ac8f3c4212685134794573
Javier Solis <javsolis@mit.edu>
2024-09-24 21:31:14 -0500

    move regex creation into separate functions

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,8 @@ fn main() {
     let filename = "src/main.rs";
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    // Regex to match any line that contains `// todo ... :` and capture the content
-    let todo_prefix = r"//\s*todo\s*";
-    let todo_content = r"(?<todo_content>.*?)";
-    let colon_separator = r"\s*:\s*";
-    let comment_content = r"(?<comment_content>.*)";
-
-    let general_todo_re = Regex::new(&format!(
-        r"{}{}{}{}",
-        todo_prefix, todo_content, colon_separator, comment_content
-    ))
-    .unwrap();
-
-    // Regex to match correctly formatted 'todo' comments
-    let keyword_pattern = r"[a-zA-Z0-9_-]+";
-    let parens_pattern = format!(r"\((?<parens>{})\)", keyword_pattern);
-    let braces_pattern = format!(r"\{{(?<braces>{})\}}", keyword_pattern);
-    let brackets_pattern = format!(r"\[(?<brackets>{})\]", keyword_pattern);
-    let angles_pattern = format!(r"<(?<angles>{})>", keyword_pattern);
-    let delimiter_pattern = format!(
-        r"(?:{}|{}|{}|{})",
-        parens_pattern, braces_pattern, brackets_pattern, angles_pattern
-    );
-    let specific_todo_re = Regex::new(&format!(r"^{}{{0,3}}$", delimiter_pattern)).unwrap();
+    let general_todo_re = create_general_todo_regex();
+    let specific_todo_re = create_specific_todo_regex();
 
     // Parse through file to match against the general regex
     for (line_number, line) in contents.lines().enumerate() {
@@ -74,4 +53,33 @@ fn main() {
             println!("\n");
         }
     }
+}
+
+fn create_general_todo_regex() -> Regex {
+    let todo_prefix = r"//\s*todo\s*";
+    let todo_content = r"(?<todo_content>.*?)";
+    let colon_separator = r"\s*:\s*";
+    let comment_content = r"(?<comment_content>.*)";
+
+    Regex::new(&format!(
+        r"{}{}{}{}",
+        todo_prefix, todo_content, colon_separator, comment_content
+    ))
+    .unwrap()
+}
+
+fn create_specific_todo_regex() -> Regex {
+    let keyword_pattern = r"[a-zA-Z0-9_-]+";
+
+    let parens_pattern = format!(r"\((?<parens>{})\)", keyword_pattern);
+    let braces_pattern = format!(r"\{{(?<braces>{})\}}", keyword_pattern);
+    let brackets_pattern = format!(r"\[(?<brackets>{})\]", keyword_pattern);
+    let angles_pattern = format!(r"<(?<angles>{})>", keyword_pattern);
+
+    let delimiter_pattern = format!(
+        r"(?:{}|{}|{}|{})",
+        parens_pattern, braces_pattern, brackets_pattern, angles_pattern
+    );
+
+    Regex::new(&format!(r"^{}{{0,3}}$", delimiter_pattern)).unwrap()
 }
