b647e6bc37f11e509c1d30c3ab3e7fddd3104515
Javier Solis <javsolis@mit.edu>
2024-10-08 14:08:25 -0500

    modify skeleton code for line_analyzer

--- src/line_analyzer.rs
+++ src/line_analyzer.rs
@@ -1,27 +1,29 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use git2::Repository;
 use regex::Regex;
+use strum::IntoEnumIterator;
 
-use crate::types::{CommentMarker, Delimiter, TodoCommentResult};
+use crate::types::{
+    CaptureGroupNames, CommentMarker, Delimiter, DelimiterContent, InvalidContent,
+    InvalidTodoComment, TodoCommentResult, ValidContent, ValidTodoComment,
+};
 
 struct GitBlameContext {
     repo: Repository, // todo: add an option to specify specific branch?
 }
 
 pub struct LineAnalyzer {
-    git_blame_context: Option<GitBlameContext>,
-    validation_regex: Regex,
+    // todo: use these attributes
+    // git_blame_context: Option<GitBlameContext>,
+    // validation_regex: Regex,
 }
 
 impl LineAnalyzer {
     pub fn new() -> Result<Self> {
-        todo!()
+        Ok(LineAnalyzer {})
     }
 
-    fn process(&self, line: &str, line_number: usize) -> Result<TodoCommentResult> {
-        todo!()
-    }
-
+    pub fn process(&self, line: &str, line_number: usize) -> Result<TodoCommentResult> {
     fn validate(marker_content: &str) -> Result<bool> {
         todo!()
     }
