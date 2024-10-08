114f5d9e9d4acc3286057faf9b0297e3c8cc8a3c
Javier Solis <javsolis@mit.edu>
2024-10-08 14:13:07 -0500

    refactor(line-analysis): integrate LineAnalyzer struct in main.rs

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use types::{
     TodoCommentResult, ValidContent, ValidTodoComment,
 };
 mod utils;
+use line_analyzer::LineAnalyzer;
 use std::path::Path;
 use walkdir::WalkDir;
 
@@ -67,10 +68,12 @@ fn analyze_file(filename: &str) -> Result<FileAnalysis> {
         invalids: Vec::new(),
     };
 
+    let line_analyzer_obj = LineAnalyzer::new()?;
+
     for (line_number, line) in reader.lines().enumerate() {
         let line = line.context("Failed to read line")?;
-        let processed_line = process_line(&line, line_number)?;
 
+        let processed_line = line_analyzer_obj.process(&line, line_number)?;
         match processed_line {
             TodoCommentResult::Valid(comment) => {
                 file_analysis.valids.push(comment);
@@ -83,148 +86,6 @@ fn analyze_file(filename: &str) -> Result<FileAnalysis> {
     }
 
     Ok(file_analysis)
-}
-
-fn process_line(line: &str, line_number: usize) -> Result<TodoCommentResult> {
-    let validation_regex = create_validation_regex(CommentMarker::Todo)
-        .context("Failed to create validation regex")?;
-
-    let general_cap = match validation_regex.captures(line) {
-        Some(cap) => cap,
-        None => return Ok(TodoCommentResult::NotApplicable),
-    };
-
-    let marker_content = general_cap
-        .name(CaptureGroupNames::MarkerContent.as_ref())
-        .map(|m| m.as_str())
-        .ok_or_else(|| {
-            anyhow::anyhow!(
-                "Missing '{}' capture group",
-                CaptureGroupNames::MarkerContent
-            )
-        })?;
-
-    let comment_content = general_cap
-        .name(CaptureGroupNames::CommentContent.as_ref())
-        .map(|m| m.as_str())
-        .ok_or_else(|| {
-            anyhow::anyhow!(
-                "Missing '{}' capture group",
-                CaptureGroupNames::CommentContent
-            )
-        })?;
-
-    let line_number = line_number + 1; // for human readable purposes
-
-    if validate_todo(marker_content).unwrap_or(false) {
-        let mut delimiters = Vec::new();
-
-        for delimiter in Delimiter::iter() {
-            if let Ok(Some(content)) = extract_delimiter_content(&delimiter, marker_content) {
-                delimiters.push(DelimiterContent {
-                    delimiter_type: delimiter.get_name().to_string(),
-                    content: content.to_string(),
-                });
-            }
-        }
-
-        Ok(TodoCommentResult::Valid(ValidTodoComment {
-            line: line_number,
-            line_info: ValidContent {
-                comment: comment_content.to_string(),
-                delimiters,
-            },
-        }))
-    } else {
-        Ok(TodoCommentResult::Invalid(InvalidTodoComment {
-            line: line_number,
-            line_info: InvalidContent {
-                full_text: general_cap[0].to_string(),
-            },
-        }))
-    }
-}
-
-fn create_validation_regex(marker: CommentMarker) -> Result<Regex> {
-    let prefix = format!(r"//\s*{}\s*", marker);
-    let marker_content = format!(r"(?<{}>.*?)", CaptureGroupNames::MarkerContent);
-    let colon_separator = r"\s*:\s*";
-    let comment_content = format!(r"(?<{}>.*)", CaptureGroupNames::CommentContent);
-
-    Regex::new(&format!(
-        r"{}{}{}{}",
-        prefix, marker_content, colon_separator, comment_content
-    ))
-    .context("Failed to create validation regex")
-}
-
-/// Validates the contents of a todo (what's between 'todo' and ':'). Returns true if valid,
-/// false otherwise.
-///
-/// Definition of a "valid" todo comment:
-/// * There is only 0 or 1 occurrence of each delimiter type:
-///   * Types: parentheses, braces, brackets, and angle brackets.
-///   * Only characters matching the standard word character class (\w) are allowed between
-///     matching delimiter characters.
-/// * The order of the delimiters doesn't matter.
-///
-
-fn validate_todo(todo_content: &str) -> Result<bool> {
-    let keyword_pattern = r".*?";
-    let delimiters = [
-        (r"\((?<parens>{})\)", "parens"),
-        (r"\{(?<braces>{})\}", "braces"),
-        (r"\[(?<brackets>{})\]", "brackets"),
-        (r"<(?<angles>{})>", "angles"),
-    ];
-
-    let mut found_delimiters = Vec::new();
-
-    for (pattern, name) in &delimiters {
-        let regex = Regex::new(&format!(r"^{}$", pattern.replace("{}", keyword_pattern)))
-            .with_context(|| format!("Failed to create regex for {}", name))?;
-
-        if let Some(captures) = regex.captures(todo_content) {
-            let value = captures
-                .name(name)
-                .ok_or_else(|| anyhow::anyhow!("Failed to get capture group"))?
-                .as_str();
-
-            println!("Captured {} content: {}", name, value);
-
-            // todo: simplify or use a variable/helper-function?
-            if value.is_empty() || Regex::new(r"[^\w]").unwrap().is_match(value) {
-                return Ok(false);
-            }
-
-            if found_delimiters.contains(name) {
-                return Ok(false); // Duplicate delimiter found
-            }
-            found_delimiters.push(name);
-        }
-    }
-
-    Ok(found_delimiters.len() <= 4)
-}
-
-/// Extracts content between specified delimiter characters in a given line of text.
-/// Returns None if no valid delimited content is found.
-fn extract_delimiter_content<'a>(delimiter: &Delimiter, line: &'a str) -> Result<Option<&'a str>> {
-    let chars = delimiter.get_chars();
-    let (open_delim, close_delim) = chars.to_tuple();
-
-    let pattern = if *delimiter == Delimiter::Angles {
-        format!(r"{}(.*?){}", open_delim, close_delim)
-    } else {
-        format!(r"\{}(.*?)\{}", open_delim, close_delim)
-    };
-
-    let re = Regex::new(&pattern).context("Failed to create regex")?;
-
-    Ok(re
-        .captures(line)
-        .and_then(|cap| cap.get(1))
-        .map(|m| m.as_str()))
 }
 
 #[cfg(test)]
