f230e9628bcbc289e590f756be0244996fcf290c
Javier Solis <javsolis@mit.edu>
2024-10-08 13:05:00 -0500

    add skeleton code for line_analyzer

--- src/line_analyzer.rs
+++ src/line_analyzer.rs
@@ -1,1 +1,39 @@
+use anyhow::Result;
+use git2::Repository;
+use regex::Regex;
 
+use crate::types::{CommentMarker, Delimiter, TodoCommentResult};
+
+struct GitBlameContext {
+    repo: Repository, // todo: add an option to specify specific branch?
+}
+
+pub struct LineAnalyzer {
+    git_blame_context: Option<GitBlameContext>,
+    validation_regex: Regex,
+}
+
+impl LineAnalyzer {
+    pub fn new() -> Result<Self> {
+        todo!()
+    }
+
+    fn process(&self, line: &str, line_number: usize) -> Result<TodoCommentResult> {
+        todo!()
+    }
+
+    fn validate(marker_content: &str) -> Result<bool> {
+        todo!()
+    }
+
+    fn extract_delimiter_content<'a>(
+        delimiter: &Delimiter,
+        line: &'a str,
+    ) -> Result<Option<&'a str>> {
+        todo!()
+    }
+
+    fn create_validation_regex(marker: CommentMarker) -> Result<Regex> {
+        todo!()
+    }
+}
