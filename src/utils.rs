e91e639d96fc3d1b0468917c738ca1601da4f249
Javier Solis <javsolis@mit.edu>
2024-10-16 13:04:56 -0500

    remove print_todo_result

--- src/utils.rs
+++ src/utils.rs
@@ -1,42 +1,6 @@
-use crate::line_analyzer_types::TodoCommentResult;
-use anyhow::Result;
 use serde_json;
 
 pub fn print_json<T: serde::Serialize>(item: &T) {
     let json = serde_json::to_string_pretty(item).unwrap();
     println!("{}\n", json);
-}
-
-pub fn print_todo_result(result: &Result<TodoCommentResult>) {
-    if let Ok(todo_result) = result {
-        match todo_result {
-            TodoCommentResult::Valid(comment) => {
-                println!("'todo' on line {}:", comment.line);
-                println!("\tIs Valid: True");
-                println!("\tComment content: {}", comment.line_info.comment);
-                println!(
-                    "\tDelimiters Found: {:?}",
-                    comment
-                        .line_info
-                        .delimiters
-                        .iter()
-                        .map(|d| d.delimiter_type.get_name())
-                        .collect::<Vec<_>>()
-                );
-                for delimiter in &comment.line_info.delimiters {
-                    println!(
-                        "\tContents of {:#?}: {}",
-                        delimiter.delimiter_type, delimiter.content
-                    );
-                }
-            }
-            TodoCommentResult::Invalid(comment) => {
-                println!("'todo' on line {}:", comment.line);
-                println!("\tIs Valid: False");
-                println!("\tFull text: {}", comment.line_info.full_text);
-            }
-            _ => {}
-        }
-        println!("\n");
-    }
 }
